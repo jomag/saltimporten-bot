19a535e8f0e866c13c84843e625d6a5ec6abf9ac
Jonatan Magnusson <jonatan.magnusson@above.se>
2019-11-07 13:11:03 +0100

    Heroku setup

--- saltimporten.py
+++ saltimporten.py
@@ -9,18 +9,18 @@ def get_todays_menu():
     soup = BeautifulSoup(response.text, "html.parser")
     current = soup.find("li", class_="current")
     meal = current.find("div", class_="meal")
-    print(meal.string)
     return meal.string
-
-get_todays_menu()
 
 app = Flask(__name__)
 
 @app.route('/')
-def meal_of_the_day():
+def index():
     try:
         meal = get_todays_menu()
         return meal
     except e:
         return "i'm not working today"
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
 
